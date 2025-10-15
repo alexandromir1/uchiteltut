302644c64e1ed5c3099228f26589e566029afb4f
IT-Achi <alexandromir3@gmail.com>
2025-10-15 15:00:52 +0900

    Fix: Add AuthProvider to fix useAuth error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-// client/src/App.jsx
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext'; // ДОБАВЬТЕ ЭТОТ ИМПОРТ
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import JobDetail from './pages/JobDetail';
 import TeacherProfile from './pages/TeacherProfile';
 import SchoolProfile from './pages/SchoolProfile';
-
 import './App.css';
-
-// const API_URL = process.env.NODE_ENV === 'production'
-//   ? 'https://uchiteltut.onrender.com/graphql'
-//   : 'http://localhost:5000/graphql';
-
-// const httpLink = createHttpLink({
-//   uri: API_URL,
-// });
 
 const API_URL = 'https://uchiteltut.onrender.com/graphql';
 
@@ -39,19 +30,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/job/:id" element={<JobDetail />} />
-            <Route path="/profile/teacher" element={<TeacherProfile />} />
-            <Route path="/profile/school" element={<SchoolProfile />} />
-
-          </Routes>
-        </div>
-      </Router>
+      <AuthProvider> {/* ДОБАВЬТЕ ЭТУ ОБЕРТКУ */}
+        <Router basename={process.env.PUBLIC_URL}>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/job/:id" element={<JobDetail />} />
+              <Route path="/profile/teacher" element={<TeacherProfile />} />
+              <Route path="/profile/school" element={<SchoolProfile />} />
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider> {/* ЗАКРОЙТЕ ОБЕРТКУ */}
     </ApolloProvider>
   );
 }
